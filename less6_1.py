8b939a900a432f54066b7b0b8fa170b550404c37
NikitaNezgovorov <Nezgovorov.nikita@gmail.com>
2020-12-22 22:05:21 +0300

    lesson 6

---
+++ less6_1.py
@@ -0,0 +1,40 @@
+import random
+
+
+class Car:
+    population = []
+
+    def __init__(self, color, year, brand, model):
+        self.color = color
+        self.year = year
+        self.brand = brand
+        self.model = model
+        self.population.append(self)
+
+    def run(self):
+        print('DRDRDR')
+
+
+class Wolf:
+    _some_protected = 'Protected'
+    __some_private = 'PRIVATE'
+    def __init__(self, color, height, sex):
+        self.color = color
+        self.height = height
+        self.sex = sex
+
+    def say(self):
+        print('RRRRRRRAAAAAYYY')
+
+
+class Dog(Wolf):
+
+    def __init__(self,name, color, height):
+        super().__init__(color, height, random.choice(('f', 'm')))
+        self.name = name
+        def say(self):
+            print('WOFFFFF')
+
+    def tic(self):
+        print(self.__some_private)
+
